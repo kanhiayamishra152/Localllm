b0a11602f4134fb391057b320428adcefdd13209
@Praxai <kanhiayamishra152@gmail.com>
2026-03-28 09:42:41 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,7 @@
-// build.gradle.kts (Project root)
 plugins {
     id("com.android.application") version "8.2.2" apply false
-    id("com.android.library") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
     id("com.google.dagger.hilt.android") version "2.50" apply false
-    kotlin("plugin.serialization") version "1.9.22" apply false
-}
-
-task("clean", Delete::class) {
-    delete(rootProject.layout.buildDirectory)
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22" apply false
 }
