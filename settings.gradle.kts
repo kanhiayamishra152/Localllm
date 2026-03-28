016f15c6e848cf0a4a8c7e99946ef2a929b022bf
@Praxai <kanhiayamishra152@gmail.com>
2026-03-28 09:00:42 +0530

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolution {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "LocalLLM"
+include(":app")
+include(":llama")
