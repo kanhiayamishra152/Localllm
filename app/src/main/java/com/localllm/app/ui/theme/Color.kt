c051fb55a9204806177f8de62739a9ca956133e5
@Praxai <kanhiayamishra152@gmail.com>
2026-03-28 09:11:23 +0530

    Create Color.kt

---
+++ app/src/main/java/com/localllm/app/ui/theme/Color.kt
@@ -0,0 +1,38 @@
+package com.localllm.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Dark theme colors (ChatGPT-style)
+val DarkBackground = Color(0xFF212121)
+val DarkSurface = Color(0xFF2F2F2F)
+val DarkSurfaceVariant = Color(0xFF424242)
+val DarkSidebar = Color(0xFF171717)
+val DarkInputBar = Color(0xFF2F2F2F)
+val DarkBorderColor = Color(0xFF565656)
+
+val UserBubbleDark = Color(0xFF2F2F2F)
+val AssistantBubbleDark = Color.Transparent
+
+val AccentGreen = Color(0xFF10A37F)
+val AccentGreenLight = Color(0xFF1ED49F)
+
+// Light theme colors
+val LightBackground = Color(0xFFFFFFFF)
+val LightSurface = Color(0xFFF7F7F8)
+val LightSurfaceVariant = Color(0xFFEFEFEF)
+val LightSidebar = Color(0xFFF9F9F9)
+val LightInputBar = Color(0xFFFFFFFF)
+val LightBorderColor = Color(0xFFE5E5E5)
+
+val UserBubbleLight = Color(0xFFF7F7F8)
+val AssistantBubbleLight = Color.Transparent
+
+val TextPrimaryDark = Color(0xFFECECEC)
+val TextSecondaryDark = Color(0xFFB4B4B4)
+val TextPrimaryLight = Color(0xFF1A1A1A)
+val TextSecondaryLight = Color(0xFF6E6E6E)
+
+val ThinkingBubbleColor = Color(0xFF1E3A5F)
+val ErrorColor = Color(0xFFEF4444)
+val WarningColor = Color(0xFFF59E0B)
+val SuccessColor = Color(0xFF10B981)
